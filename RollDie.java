e79e9bd3738cfba3f5532d6bf2d28fe750b03d79
rushikeshbhandare <86002633+rushikeshbhandare97@users.noreply.github.com>
2021-10-04 22:49:03 +0530

    Added: Roll Die program is Added

---
+++ RollDie.java
@@ -0,0 +1,8 @@
+public class RollDie {
+    public static void main(String[] args) {
+        int SIDES = 6;  
+        int roll = (int) (Math.random() * SIDES) + 1;
+        System.out.println(roll);
+    }
+
+}
